55ea04dc06aae06b94b2ec9a161402f1610a158d
oushesh007 <oushesh@concular.com>
2022-06-21 11:46:17 +0200

    updated the listening to the port on github

--- listening_to_github.py
+++ listening_to_github.py
@@ -11,7 +11,9 @@ def api_route():
 @app.route('/github',methods=['POST'])
 def api_gh_mesage():
     if request.headers['Content-Type']=="application/json":
-        return json.dumps(request.json)
+        my_info = json.dumps(request.json)
+        print (my_info)
+        return my_info
 
 #similar to django export
 if __name__ == '__main__':
